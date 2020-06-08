deadb9ee1902ebd84034c6b6aba3033af7200e48
Lukas Berglund <lukastberglund@gmail.com>
2020-06-08 17:34:37 +0000

    add super simple map page

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -15,6 +15,5 @@
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16}
-      );
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
 }
