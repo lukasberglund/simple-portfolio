0112d31020bf93e25991ba76cab8da9d05b88b3e
Lukas Berglund <lukastberglund@gmail.com>
2020-06-12 16:46:56 +0000

    fixup! change language to be as broad as possible

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -40,7 +40,7 @@ function setLanguageLabel(country) {
     language = languageMap[country];
   }
 
-  languageLabel.innerText = 'Official language(s): ' + language;
+  languageLabel.innerText = 'Languages commonly spoken: ' + language;
 }
 
 function determineCountry(address) {
